72ad02ae3998d44d541afd99bf8ecd018e5bd0f3
Артем  Мастренков <artemmastrenkov@MacBook-Pro-Artem.local>
2020-08-13 12:56:53 +0500

    1.0.2

---
+++ social/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TodoList from "./Todo/TodoList";
+
+function App() {
+  const todos = [
+    { id: 1, completed: false, title: "Купить хлеб" },
+    { id: 2, completed: false, title: "Купить молока" },
+    { id: 3, completed: false, title: "Убраться в комнате" },
+  ];
+
+  return (
+    <div className="wrapper">
+      <h1>React App</h1>
+
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
